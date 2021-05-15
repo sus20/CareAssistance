a9587cc97d729eef0dfd02a2b3a2184fa46a4564
Sushma <susma.sapkota5@gmail.com>
2021-05-15 18:03:24 +0200

    replaced video player with Better_Player.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:refresh/screens/ConversationPartner.dart';
+import 'package:refresh/screens/ExerciseView.dart';
 import 'package:refresh/screens/HelloScreen.dart';
 import 'package:refresh/screens/HomeScreen.dart';
+import 'package:refresh/screens/WhatsUpScreen.dart';
 
-
-
-void main()  => runApp(MaterialApp(
-  initialRoute: '/',
-  routes: {
-    // '/': (context) => HomeScreen(key: UniqueKey(),title: '',),
-    '/HelloScreen' : (context) => HelloScreen(key:  UniqueKey()),
-    '/' : (context) => ConversationPartner(),
-  },
-
-));
+void main() => runApp(MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/HomeScreen': (context) => HomeScreen(
+              key: UniqueKey(),
+              title: '',
+            ),
+        '/HelloScreen': (context) => HelloScreen(key: UniqueKey()),
+        '/WhatsUpScreen': (context) => WhatsUpScreen(),
+        '/ConversationPartner': (context) => ConversationPartner(),
+        '/': (context) => ExerciseView(key: UniqueKey()),
+      },
+    ));
--- lib/screens/ExerciseView.dart
+++ lib/screens/ExerciseView.dart
@@ -1,36 +1,25 @@
-
+import 'package:better_player/better_player.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
 class ExerciseView extends StatefulWidget {
-
-  ExerciseView({required Key key}): super(key: key);
+  ExerciseView({required Key key}) : super(key: key);
 
   @override
   _ExerciseViewState createState() => _ExerciseViewState();
 }
 
 class _ExerciseViewState extends State<ExerciseView> {
-   late VideoPlayerController _controller;
-   late Future<void> _initializeVideoPlayerFuture;
+  late VideoPlayerController _controller;
+  late Future<void> _initializeVideoPlayerFuture;
 
-  @override
-  void initState() {
-    String videoUrl = "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4";
-    _controller = VideoPlayerController.network(videoUrl);
-    _initializeVideoPlayerFuture = _controller.initialize();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
 
   @override
   Widget build(BuildContext context) {
     double videoPlayerScreenScale = 5.8;
+    String videoUrl =
+        "https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4";
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Übungsansicht'),
@@ -39,39 +28,35 @@ class _ExerciseViewState extends State<ExerciseView> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        padding: EdgeInsets.all(20),
-        height: 40.0 * videoPlayerScreenScale,
-        width: (40.0 * videoPlayerScreenScale * 16) / 9 ,
-        child: FutureBuilder(
-          future: _initializeVideoPlayerFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              return AspectRatio(aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              );
-            } else {
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
-        ),
-      ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            if(_controller.value.isPlaying) {
-              _controller.pause();
-            } else {
-              _controller.play();
-            }
-          });
-        },
-        child: Icon(_controller.value.isPlaying? Icons.pause: Icons.play_arrow),
-      ),
-
-
+          padding: EdgeInsets.all(10),
+          child: AspectRatio(
+            aspectRatio: 16 / 9,
+            child: BetterPlayerPlaylist(
+             betterPlayerConfiguration: BetterPlayerConfiguration(),
+              betterPlayerPlaylistConfiguration: BetterPlayerPlaylistConfiguration(),
+              betterPlayerDataSourceList: createDataSet(),
+            ),
+          )),
     );
   }
+
+  List<BetterPlayerDataSource> createDataSet() {
+    List<BetterPlayerDataSource> dataSourceList = [];
+    dataSourceList.add(
+      BetterPlayerDataSource(
+        BetterPlayerDataSourceType.network,
+        "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+      ),
+    );
+    dataSourceList.add(
+      BetterPlayerDataSource(BetterPlayerDataSourceType.network,
+          "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"),
+    );
+    dataSourceList.add(
+      BetterPlayerDataSource(BetterPlayerDataSourceType.network,
+          "http://sample.vodobox.com/skate_phantom_flex_4k/skate_phantom_flex_4k.m3u8"),
+    );
+    return dataSourceList;
+  }
 }
+
